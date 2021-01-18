a43252d8c7d2810a987bfcc9773a5c64bb45f74f
Anubhav27-hack <anubhavborkotoky27#gmail.com>
2021-01-19 01:56:28 +0530

    Variables

---
+++ js-2.js
@@ -0,0 +1,10 @@
+//Old
+var Variable1 = "I";
+//new
+let Variable2 = "Am";
+const Variable3 = "Trash at Valo";
+Say1 = Variable1 + Variable2 + Variable3;
+Say2 = `${Variable1} ${Variable2} not ${Variable3}`;
+console.log(Say1);
+console.log(Say2);
+console.log(Say1 === Say2);
